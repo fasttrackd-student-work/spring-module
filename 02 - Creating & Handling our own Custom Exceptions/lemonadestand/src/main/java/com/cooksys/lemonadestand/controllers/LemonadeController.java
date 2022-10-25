76bf91b17a14229557f124675aa767873174739a
Will Marttala <wmarttala123@gmail.com>
2022-10-25 14:47:31 -0500

    Added PowerPoint, lemonadestand project, postman collection to 02 video folder

---
+++ 02 - Creating & Handling our own Custom Exceptions/lemonadestand/src/main/java/com/cooksys/lemonadestand/controllers/LemonadeController.java
@@ -0,0 +1,43 @@
+package com.cooksys.lemonadestand.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.lemonadestand.model.LemonadeRequestDto;
+import com.cooksys.lemonadestand.model.LemonadeResponseDto;
+import com.cooksys.lemonadestand.services.LemonadeService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/lemonades")
+@AllArgsConstructor
+public class LemonadeController {
+
+    private LemonadeService lemonadeService;
+
+    @GetMapping
+    public List<LemonadeResponseDto> getAllLemonades() {
+        return lemonadeService.getAllLemonades();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public LemonadeResponseDto createLemonade(@RequestBody LemonadeRequestDto lemonadeRequestDto) {
+        return lemonadeService.createLemonade(lemonadeRequestDto);
+    }
+
+    @GetMapping("/{id}")
+    public LemonadeResponseDto getLemonadeById(@PathVariable Long id) {
+        return lemonadeService.getLemonadeById(id);
+    }
+
+}
